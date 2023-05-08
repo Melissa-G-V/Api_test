8c0ad1fc3564d4dd6d3ada311f1a7504d8a4d5db
Juan Schiavon <juanschiavon@hotmail.com>
2023-05-08 18:35:02 -0300

    ADD/CRUD, Model e Routes de Paciente.

--- routes.js
+++ routes.js
@@ -1,30 +1,17 @@
 import { Router } from "express";
-import {
-  usuarioCreate,
-  usuarioIndex,
-  admin_CreateAdm,
-  usuarioDelete,
-  usuarioUpdate,
-} from "./Controllers/usuarioController.js";
-import {
-  medicamentoCreate,
-  medicamentoIndex,
-  medicamentoUpdate,
-  medicamentoDelete,
-  pesqNomeMedicamento
-} from "./Controllers/medicamentoController.js"
+import { usuarioCreate, usuarioIndex, admin_CreateAdm, usuarioDelete, usuarioUpdate} from "./Controllers/usuarioController.js";
+import { medicamentoCreate, medicamentoIndex, medicamentoUpdate, medicamentoDelete, pesqNomeMedicamento} from "./Controllers/medicamentoController.js"
+import { pacienteCreate, pacienteDelete, pacienteIndex, pacienteUpdate, pesqPacienteNome } from "./Controllers/pacienteController.js";
 
 const router = Router();
-//usuarios
+
 router
   .get("/usuarios", usuarioIndex)
   .post("/usuario/create", usuarioCreate)
   .put("/usuario/update/:pesq", usuarioUpdate)
   .delete("/usuario/delete/:id", usuarioDelete);
-// rota admin
-router.post("/usuario/cria/admin", admin_CreateAdm);
 
-//medicamento
+router.post("/usuario/create/admin", admin_CreateAdm);
 
 router
   .get("/med", medicamentoIndex)
@@ -32,5 +19,12 @@ router
   .put("/med/update/:pesq", medicamentoUpdate)
   .delete("/med/delete/:id", medicamentoDelete)
   .get('/med/pesq/:pesq%', pesqNomeMedicamento)
+
+router
+  .get('/pacientes', pacienteIndex)
+  .post('/pacientes', pacienteCreate)
+  .delete('/pacientes', pacienteDelete)
+  .put('/pacientes', pacienteUpdate)
+  .get('/pacientes/:nome', pesqPacienteNome)
 
 export default router;
