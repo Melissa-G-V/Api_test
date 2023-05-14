512440534bfb784358ac502c2ad47a54add6453e
veronicaschiller <veronicaschiller17@gmail.com>
2023-05-14 20:32:10 -0300

    🚧 construção de crud de responsavel

--- Controllers/responsavelController.js
+++ Controllers/responsavelController.js
@@ -47,3 +47,25 @@ export const responsavelUpdate = async (req , res) =>{
         res.status(400).json(error)
     }
 }
+
+export const responsavelPesq = async (req , res) => {
+    const {pesq} = req.params
+    const{nome, grauDeParentesco,contato}= req.body
+    if(!nome || !grauDeParentesco || !contato){
+        res.status(400).json({id:0 , msg: "Responsavel n encontrado"})
+    }
+    try {
+        const responsavel = await Responsavel.findAll({
+            nome,
+            grauDeParentesco,
+            contato
+        },{
+            where:{
+                nome:{[Op.substring]: pesq},
+            }
+        })
+        res.status(200).json(responsavel)
+    } catch (error) {
+    res.status(403).json(error)  
+    }
+}
